0e2cd04c59c7d65979c7dd6890559b37ad3b6eb3
Sayem Shafayet <ishafayet@gmail.com>
2024-09-13 15:49:34 +0600

    Create LifeKeeper.ts

---
+++ src/objects/LifeKeeper.ts
@@ -0,0 +1,30 @@
+import { CANVAS_BASE_HEIGHT, CANVAS_BASE_WIDTH, MAX_PLAYER_LIVES } from "../constants";
+import { Game } from "./Game";
+
+export class LifeKeeper {
+  game: Game;
+
+  remainingLives: number;
+
+  constructor(game: Game) {
+    this.game = game;
+    this.remainingLives = MAX_PLAYER_LIVES;
+  }
+
+  reduceLife() {
+    if (this.remainingLives > 0) {
+      this.remainingLives -= 1;
+    }
+
+    if (this.remainingLives === 0){
+      this.game.triggerGameOver();
+    }
+  }
+
+  draw(ctx: CanvasRenderingContext2D) {
+    let text = "❤️".repeat(this.remainingLives);
+    ctx.font = "normal 20px Courier New";
+    ctx.fillStyle = "grey";
+    ctx.fillText(text, 60, 120);
+  }
+}
